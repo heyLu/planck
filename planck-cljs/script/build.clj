96de5d2e3c91570e5f0ee8807867f8a74052ce4f
Mike Fikes <mike@fikesfarm.com>
2016-05-31 11:37:23 -0400

    Add cljs.spec.test ns

--- planck-cljs/script/build.clj
+++ planck-cljs/script/build.clj
@@ -40,6 +40,8 @@
 
 (copy-source "cljs/test.cljc")
 (copy-source "cljs/spec.cljc")
+(copy-source "cljs/spec/test.cljc")
+(copy-source "cljs/spec/test.cljs")
 (copy-source "cljs/spec/impl/gen.cljc")
 (copy-source "cljs/analyzer/api.cljc")
 (copy-source "clojure/template.clj")
