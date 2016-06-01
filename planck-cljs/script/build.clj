23d7fcfa66653fba5825b598f3d93dafec103e59
Mike Fikes <mike@fikesfarm.com>
2016-06-01 14:07:14 -0400

    Update to 1.9.14

--- planck-cljs/script/build.clj
+++ planck-cljs/script/build.clj
@@ -23,7 +23,7 @@
        (when-let [s (cljs.analyzer/error-message warning-type extra)]
          (binding [*out* *err*]
            (println "WARNING:" (cljs.analyzer/message env s)))
-         (System/exit 1))))]
+         #_(System/exit 1))))]
   (api/build (api/inputs "src")
     {:output-dir         "out"
      :output-to          "out/main.js"
